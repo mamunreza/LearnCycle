4442e5ff5279fb2a9aa0c7e44258b7d43ec99c1d
Mamun Reza <mamun.reza09@gmail.com>
2020-08-13 14:53:26 +0300

    simple masstransit things (#4)
    Co-authored-by: Mamun Reza <MuhammadMamun.Reza@arvato.com>

---
+++ SenderConsole/BusConfigurator.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenderConsole
+{
+    public static class BusConfigurator
+    {
+        public static IBusControl ConfigureBus(
+            Action<IRabbitMqBusFactoryConfigurator> configureAction = null)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(configure =>
+            {
+                configure.ConfigureRabbitMq();
+                configureAction?.Invoke(configure);
+            });
+        }
+
+        public static void ConfigureRabbitMq(this IRabbitMqBusFactoryConfigurator configure)
+        {
+            // TODO: Configure redelivery 
+            var host = ConfigureHost(configure);
+
+            //ConfigureMessageScheduler(configure, host, rabbitMqConfiguration);
+            configure.PrefetchCount = 8;
+        }
+
+        private static IRabbitMqHost ConfigureHost(IRabbitMqBusFactoryConfigurator configure)
+        {
+            var virtualHost = string.IsNullOrEmpty(RabbitMqConstant.VirtualHost) ? "/" : RabbitMqConstant.VirtualHost;
+            var host = configure.Host(RabbitMqConstant.HostName, RabbitMqConstant.Port, virtualHost, c =>
+            {
+                c.Username(RabbitMqConstant.UserName);
+                c.Password(RabbitMqConstant.Password);
+               
+            });
+            return host;
+        }   
+    }
+}
---
+++ SenderConsole/RabbitMqConstant.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SenderConsole
+{
+    public class RabbitMqConstant
+    {
+        public const string HostName = "localhost";
+        public const string VirtualHost = "dev";
+        public const string Port = "15672";
+        //public const string RabbitMqUri = "http://localhost:15672/";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string SimpleServiceQueue = "SimpleService";
+
+        //public const List<string> ClusterNodesList = new List<string>();
+    }
+}
