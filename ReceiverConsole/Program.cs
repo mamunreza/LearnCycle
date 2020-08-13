dbe6cf47ec3701133c5c759feb2905e416bda6b4
Mamun Reza <MuhammadMamun.Reza@arvato.com>
2020-08-13 14:51:35 +0300

    simple masstransit things

---
+++ ReceiverConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReceiverConsole
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.ReceiveEndpoint("event-listener-customer", e =>
+                {
+                    e.Consumer<CustomerConsumer>();
+                });
+                cfg.ReceiveEndpoint("event-listener-address", e =>
+                {
+                    e.Consumer<AddressConsumer>();
+                });
+            });
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+                await Task.Run(() => Console.ReadLine());
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+
+        public class CustomerConsumer :
+            IConsumer<Customer>
+        {
+            public async Task Consume(ConsumeContext<Customer> context)
+            {
+                Console.WriteLine("Value: {0}", context.Message.Name);
+                await Task.CompletedTask;
+            }
+        }
+        public class AddressConsumer :
+            IConsumer<Address>
+        {
+            public async Task Consume(ConsumeContext<Address> context)
+            {
+                Console.WriteLine("Value: {0}", context.Message.City);
+                await Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ SenderConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using Common;
+using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SenderConsole
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            //var busControl = BusConfigurator.ConfigureBus();
+
+            
+            var busControl = Bus.Factory.CreateUsingRabbitMq();
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+            try
+            {
+                do
+                {
+                    string value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter message (or quit to exit)");
+                        Console.Write("> ");
+                        return Console.ReadLine();
+                    });
+
+                    if ("q".Equals(value, StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        await busControl.Publish<Customer>(new
+                        {
+                            Name = $"Customer: {i}"
+                        });
+
+                        await busControl.Publish<Address>(new
+                        {
+                            City = $"Living in {i} street"
+                        });
+                    }
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
