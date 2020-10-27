91f848963ef4c1e8322da9a2735fad742be59935
Mamun Reza <mamun.reza09@gmail.com>
2020-10-27 11:27:49 +0200

    Basic (#6)
    Co-authored-by: Mamun Reza <MuhammadMamun.Reza@arvato.com>

---
+++ EmailSender.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace EmailSenderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send email start");
+            Execute().Wait();
+            Console.WriteLine("Send email end");
+            Console.ReadKey();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = @"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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("mamunrezadev@gmail.com", "Ant");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("mamun.reza09@gmail.com", "Mamun Reza");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
